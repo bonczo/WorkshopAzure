aac2e15243e60f65069b298312efc8101bc863f5
DESKTOP-MUISLJB\BARTEK <bartipach@gmail.com>
2018-02-12 20:30:34 +0100

    Added scripts

---
+++ Scripts/BasicRoles.ps1
@@ -0,0 +1,11 @@
+﻿Login-AzureRmAccount
+
+Get-AzureRmRoleDefinition | Select-Object Name
+
+Get-AzureRmRoleDefinition -Name 'Owner'
+
+Get-AzureRmRoleDefinition -Name 'Contributor'
+
+Get-AzureRmRoleDefinition -Name 'Reader'
+
+(Get-AzureRmRoleDefinition -Name 'Virtual Machine Contributor').Actions
---
+++ Scripts/CustomRole.ps1
@@ -0,0 +1,27 @@
+﻿
+Login-AzureRmAccount
+
+$subscriptionID = (Get-AzureRmSubscription | `
+Where State -EQ 'Enabled' | `
+Select -First 1).Id
+
+$role = Get-AzureRmRoleDefinition "Virtual Machine Contributor"
+
+$role.Id = $null
+$role.Name = "VM Operator"
+$role.Description = "Can monitor and restart virtual machines."
+$role.IsCustom = 'True'
+$role.Actions.Clear()
+$role.Actions.Add("Microsoft.Storage/*/read")
+$role.Actions.Add("Microsoft.Network/*/read")
+$role.Actions.Add("Microsoft.Compute/*/read")
+$role.Actions.Add("Microsoft.Compute/virtualMachines/start/action")
+$role.Actions.Add("Microsoft.Compute/virtualMachines/restart/action")
+$role.Actions.Add("Microsoft.Authorization/*/read")
+$role.Actions.Add("Microsoft.Resources/subscriptions/resourceGroups/read")
+$role.Actions.Add("Microsoft.Insights/alertRules/*")
+$role.Actions.Add("Microsoft.Support/*")
+$role.AssignableScopes.Clear()
+$role.AssignableScopes.Add("/subscriptions/$subscriptionID")
+
+New-AzureRmRoleDefinition -Role $role
